5d56415c2e61e062f6a9622da7e4eb0fa17f7da3
AssisrMatheus <assisr.matheus@gmail.com>
2019-09-15 10:09:04 -0300

    :sparkles: Added HybridScroll with responsiveness :tada: :tada:

--- components/TodoChecklister.lua
+++ components/TodoChecklister.lua
@@ -11,12 +11,70 @@ local ResponsiveFrame = core.ResponsiveFrame
 --------------------------------------
 -- TodoChecklisterFrame functions
 --------------------------------------
+function TodoChecklisterFrame:OnUpdate()
+  local todoList = {
+		{text="Item one",    isChecked=false},
+		{text="Item Two",    isChecked=false},
+		{text="Item Three",  isChecked=true},
+		{text="Item four",   isChecked=false}
+	}
+
+	local scrollFrame = TodoItemsScrollFrame
+	if (scrollFrame and scrollFrame.buttons) then
+		local offset = HybridScrollFrame_GetOffset(scrollFrame)
+
+		for i=1, #scrollFrame.buttons do
+			local idx = i + offset
+			local button = scrollFrame.buttons[i]
+
+			if ( idx <= #todoList ) then
+				local todoItem = todoList[idx]
+
+				button.todoItem = todoItem
+				button.TodoContent:SetText(todoItem.text)
+
+				button:Show()
+			else
+				button:Hide()
+			end
+		end
+		
+		HybridScrollFrame_Update(scrollFrame, (scrollFrame.buttons[1]:GetHeight()) * #todoList, scrollFrame:GetHeight())
+	end
+end
+
 function TodoChecklisterFrame:OnLoad(frame)
+  self.frame = frame
+  -- Parent's OnLoad Function
   ResponsiveFrame:OnLoad(frame)
+
+  -- Set up elements
   frame.Title:SetText(Constants.addonName)
+  
+	local scrollFrame = frame.ScrollFrame
+	scrollFrame.update = function() TodoChecklisterFrame:OnUpdate() end
+	HybridScrollFrame_CreateButtons(frame.ScrollFrame, "TodoItemTemplate")
+
+  -- Display the frame
   frame:Show()
+end
+
+function TodoChecklisterFrame:OnShow(frame)
+  self:OnUpdate()
+end
+
+function TodoChecklisterFrame:OnSizeChanged(frame)
+  self:OnUpdate()
 end
 
 function OnLoad(frame)
   TodoChecklisterFrame:OnLoad(frame)
+end
+
+function OnShow(frame)
+  TodoChecklisterFrame:OnShow(frame)
+end
+
+function OnSizeChanged(frame)
+  TodoChecklisterFrame:OnSizeChanged(frame)
 end
